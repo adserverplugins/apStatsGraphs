160f64552a8f74ee2711cff70d4bdd461fffb890
Matteo Beccati <matteo@beccati.com>
2014-05-04 14:16:15 +0200

    New phone-back cod

--- www/admin/plugins/apStatsGraphsUI/index.php
+++ www/admin/plugins/apStatsGraphsUI/index.php
@@ -49,11 +49,11 @@ function getPluginVersion()
     </div>
     <div style="margin: 4px">
         <script type='text/javascript'><!--//<![CDATA[
-var m3_u = (location.protocol=='https:'?'https://openx.adserverplugins.com/live/www/delivery/ajs.php':'http://openx.adserverplugins.com/live/www/delivery/ajs.php');
+var m3_u = (location.protocol=='https:'?'https://servedby.reviveservers.com/ajs.php':'http://servedby.reviveservers.com/ajs.php');
 var m3_r = Math.floor(Math.random()*99999999999);
 if (!document.MAX_used) document.MAX_used = ',';
 document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-document.write ("?zoneid=1&version=<?php echo urlencode(getPluginVersion()); ?>");
+document.write ("?zoneid=684&version=<?php echo urlencode(getPluginVersion()); ?>");
 document.write ('&amp;cb=' + m3_r);
 if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
 document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
@@ -62,7 +62,6 @@ if (document.referrer) document.write ("&amp;referer=" + escape(document.referre
 if (document.context) document.write ("&context=" + escape(document.context));
 if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
 document.write ("'><\/scr"+"ipt>");
-
         //]]>--></script>
     </div>
 
